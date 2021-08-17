10accafc0ce563e9b1bcbfe1c9499467eb2b9d87
Lim_yeram0629 <ysyr0830@gmail.com>
2021-08-17 17:12:32 +0900

    fist Commit

---
+++ src/com/greedy/test/TestClass.java
@@ -0,0 +1,16 @@
+package com.greedy.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		
+		/*
+		 * add : 프로젝트 파일로 부터 변경된 내용을 stagging area에 올리는 작업
+		 * commit : 본인의 local repository 로 stagging area에 있던 것을 저장하는 작업
+		 * push : local repository에 있더 ㄴ내용을 remote repository로 올리는 작업
+		 * merge: local repository에 있는 변경사항을 프로젝트에 직접 합치면서 변경하는 작업
+		 * pull : fetch와 merge를 한번에 처리할 때
+		 * */
+	}
+}
